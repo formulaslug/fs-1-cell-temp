8e3baca46ce5f01e2d1b40fa245bd6b1e3887e9d
Tyler Veness <calcmogul@gmail.com>
2017-08-23 15:25:44 -0700

    Cleaned up .styleguide and ran formatter

--- CanOpenPdo.h
+++ CanOpenPdo.h
@@ -38,7 +38,7 @@ constexpr uint32_t kCobIdS2p = 0x622;  // changed from 0x014
 constexpr uint32_t kPayloadHeartbeat = 0x1;
 
 struct HeartbeatMessage : public CANTxFrame {
-  HeartbeatMessage(uint32_t id);
+  explicit HeartbeatMessage(uint32_t id);
 };
 
 struct CellTempMessage : public CANTxFrame {
