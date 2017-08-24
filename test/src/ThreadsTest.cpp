658d6aeef8a408fcc128dc922cb8f30b6dedb4c9
Tyler Veness <calcmogul@gmail.com>
2017-08-23 17:38:15 -0700

    Added Google Test files

---
+++ test/src/ThreadsTest.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 Formula Slug. All Rights Reserved.
+
+#include "gtest/gtest.h"
+
+class ThreadsTest : public testing::Test {
+ protected:
+  void SetUp() override {}
+
+  void TearDown() override {}
+};
+
+TEST_F(ThreadsTest, AdcToTempTest) {}
