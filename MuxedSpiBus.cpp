c81042c4c48d3acccb6a7b6ea42f88793f0f0b9a
mmwebster <milo.webster@gmail.com>
2017-10-20 10:56:39 -0700

    Added MuxedSpiBus lib

---
+++ MuxedSpiBus.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) 2016-2017 Formula Slug. All Rights Reserved.
+
+#include "MuxedSpiBus.h"
+
+#include <cmath>
+#include <cstdio>
+
+/* Hard coded to baudrate=140.625kHz, CPHA=0, CPOL=0, MSb first
+ * MCP3008's baud maxes out around 3-4MHz
+ */
+constexpr SPIConfig MakeConfig(SpiBusBaudRate baud, uint8_t ssPin) {
+  uint16_t cr1 = 0, cr2 = 0;
+
+  switch (baud) {
+    case SpiBusBaudRate::k140k:
+      cr1 = SPI_CR1_BR_2 | SPI_CR1_BR_1;
+      cr2 = SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0;
+      break;
+    default:
+      break;
+  }
+
+  // slave selects hard-coded to port A
+  return {NULL, GPIOA, ssPin, cr1, cr2};
+}
+
+MuxedSpiBus::MuxedSpiBus(SpiBusBaudRate baud, const uint8_t* slavePins,
+               const uint8_t numSlaves) {
+  // init private vars from params
+  m_numSlaves = numSlaves;
+
+  // init SPI pins
+  // SCK
+  palSetPadMode(GPIOA, 5, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+  // MISO
+  palSetPadMode(GPIOA, 6, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+  // MOSI
+  palSetPadMode(GPIOA, 7, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);
+  // SS pins
+  palSetPadMode(GPIOA, slavePins[0],
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  palSetPadMode(GPIOA, slavePins[1],
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  palSetPadMode(GPIOA, slavePins[2],
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+  palSetPadMode(GPIOA, slavePins[3],
+                PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+
+  // create all config structs for each slave
+  for (uint8_t slaveIndex = 0; slaveIndex < m_numSlaves; ++slaveIndex) {
+    m_slaveConfigs[slaveIndex] = MakeConfig(baud, slavePins[slaveIndex]);
+  }
+}
+
+MuxedSpiBus::~MuxedSpiBus() {
+  spiUnselect(&SPID1);    // de-assert chip select
+  spiReleaseBus(&SPID1);  // release ownership of bus as master
+}
+
+/*
+ * @desc Reads `length` bytes from txbuf and sends over SPI bus
+ */
+void MuxedSpiBus::send(uint8_t length, const void* txbuf) {
+  spiSend(&SPID1, length, txbuf);  // transmit
+}
+
+/*
+ * @desc Writes `length` bytes into rxbuf from SPI bus
+ */
+void MuxedSpiBus::recv(uint8_t length, void* rxbuf) {
+  spiReceive(&SPID1, length, rxbuf);  // receive
+}
+
+/*
+ * @desc Acquire bus for the passed slave select pin index
+ */
+void MuxedSpiBus::acquireSlave(uint8_t ssPinIndex) {
+  spiAcquireBus(&SPID1);  // acquire ownership of the bus.
+  spiStart(&SPID1,
+           &(m_slaveConfigs[ssPinIndex]));  // setup transfer parameters.
+  spiSelect(&SPID1);                        // slave Select assertion.
+}
+
+/*
+ * @desc Release bus
+ */
+void MuxedSpiBus::releaseSlave() {
+  spiUnselect(&SPID1);    // de-assert chip select
+  spiReleaseBus(&SPID1);  // release ownership of bus as master
+}
---
+++ MuxedSpiBus.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2016-2017 Formula Slug. All Rights Reserved.
+
+#pragma once
+
+#include <initializer_list>
+
+#include "hal.h"
+
+constexpr uint8_t kSPImaxSlaves = 8;
+
+enum class SpiBusBaudRate : uint8_t {
+  k140k,  // 140.625 kHz
+};
+
+class MuxedSpiBus {
+ public:
+  SpiBus(SpiBusBaudRate baud,
+         const uint8_t* slavePins, const uint8_t numSlaves);
+  ~SpiBus();
+  void send(uint8_t length, const void* txbuf);
+  void recv(uint8_t length, void* rxbuf);
+  void acquireSlave(uint8_t ssPinIndex);
+  void releaseSlave();
+
+ private:
+  uint8_t m_numSlaves;
+  SPIConfig m_slaveConfigs[kSPImaxSlaves];
+};
